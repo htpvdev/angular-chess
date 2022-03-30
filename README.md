83081b128385e831e974deaa89bdbf6bd2be72ab
htpvdev <haruki20211121@gmail.com>
2022-03-30 23:38:18 +0900

    とほほのAngular入門コピペ

--- README.md
+++ README.md
@@ -6,11 +6,12 @@
 
 - 以下のコマンドを実行して、Angular CLIをインストールする。
   - `npm install -g @angular/cli`
-  - 参考：https://tocie.net/blog/2020/07/11/ng-newで現在地にインストールする方法/
 
 
 1. 以下のコマンドを実行して、カレントディレクトリにAngularアプリ？をインストール
   - `ng new chess-app --directory ./`
+  - 参考：https://tocie.net/blog/2020/07/11/ng-newで現在地にインストールする方法/
+
 2. そしたら、こんな感じのメッセージが出てきて、入力を求められる(これは、複数ページ作る場合はyの方がいいのかな？といった感じ)
 ```
 ? Would you like to add Angular routing? (y/N)
@@ -26,4 +27,12 @@
   Sass   [ https://sass-lang.com/documentation/syntax#the-indented-syntax ] 
   Less   [ http://lesscss.org 
 ```
+
+ローカルホスト立ち上げコマンド(http://localhost:4200)
+- `npm start`
+(ポートを変えるには`ng serve --port 80`みたいにするみたい)
+
+
+## 学習したことメモ
+
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserService } from './user/user.service';
+import { UserListComponent } from './user/user-list/user-list.component';
+import { UserEditComponent } from './user/user-edit/user-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    DashboardComponent,
+    UserListComponent,
+    UserEditComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    UserService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
